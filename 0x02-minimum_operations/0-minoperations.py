9dc8b93a18d87d1b957b1214d41d80cb4e3fc608
unknown <lizosazi@gmail.com>
2024-06-27 16:57:46 +0200

    fiixed mixed up variable

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,9 +9,9 @@ def minOperations(n):
     Returns:
         Integer : if n is impossible to achieve, return 0
     '''
-    pastedChars = 1  # how many chars in the file
-    inCopy = 0  # how many H's copied
-    counter = 0  # operations counter
+    pastedChars = 1  
+    inClipBoard = 0  
+    counter = 0  
 
     while pastedChars < n:
         if inClipBoard == 0:
@@ -19,37 +19,23 @@ def minOperations(n):
             counter += 1
 
         if pastedChars == 1:
-            # paste
             pastedChars += inClipBoard
-            # increment operations counter
             counter += 1
-            # continue to next loop
             continue
 
-        notInClipBoard = n - pastedChars  # remaining chars to Paste
-        # check if impossible by checking if clipboard
-        # has more than needed to reach the number desired
-        # which also means num of chars in file is equal
-        # or more than in the clipboard.
-        # in both situations it's impossible to achieve n of chars
+        notInClipBoard = n - pastedChars
         if notInClipBoard < inClipBoard:
             return 0
 
-        # if can't be devided
+
         if notInClipBoard % pastedChars != 0:
-            # paste current clipboard
             pastedChars += inClipBoard
-            # increment operations counter
             counter += 1
         else:
-            # copyall
             inClipBoard = pastedChars
-            # paste
             pastedChars += inClipBoard
-            # increment operations counter
             counter += 2
 
-    # if got the desired result
     if pastedChars == n:
         return counter
     else:
