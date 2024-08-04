682a6cbe4b3feccd0815ae8998e7fbbbe4f7909c
unknown <lizosazi@gmail.com>
2024-08-04 15:09:32 +0200

    making change file

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""this modules makes change from coins"""
+
+
+def makeChange(coins, total):
+    """determines and returns fewest number of coins needed to meet total"""
+    if total <= 0:
+        return (0)
+
+    #sort coins
+    coins = sorted(coins)[::-1]
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        while total >= coin:
+            total -= coin
+            change += 1
+    if total:
+        return (-1)
+    return (change)
