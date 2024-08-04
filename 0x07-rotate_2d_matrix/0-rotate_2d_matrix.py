3fcc415f8f9f6849a963bab0505c65496ed5f226
unknown <lizosazi@gmail.com>
2024-08-04 13:52:01 +0200

    initial files

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Rotates a 2D Matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """rotates a 2D matrix 90 degrees clockwise"""
+    length = len(matrix)
+    for x in range(0, int(length / 2)):
+        for y in range(x, length - x - 1):
+            temp = matrix[x][y]
+            matrix[x][y] = matrix[length - 1 - y][x]
+            matrix[length - 1 - y][x] = matrix[length - 1 - x][length - 1 - y]
+            matrix[length - 1 - x][length - 1 - y] = matrix[y][length - 1 - x]
+            matrix[y][length - 1 - x] = temp
