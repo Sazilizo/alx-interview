3c167471ec1eb68ded18cd882daca8e3a83075ba
unknown <lizosazi@gmail.com>
2024-08-04 17:16:46 +0200

    added README.md and 0-nqueens.py

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""N queens"""
+import sys
+
+number = sys.argv
+if len(number) != 2:
+    print(f"Usage: nqueens N")
+    exit(1)
+
+if not number[1].isdigit():
+    print("N must be a number")
+    exit(1)
+
+if int(number[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+k = int(number[1])
+
+
+def queens(k, i=0, a=[], b=[], c=[]):
+    """discovering all possible positions"""
+    if i < k:
+        for j in range(k):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(k, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+
+def NQueens(k):
+    """a final solution"""
+    buffer = []
+    c = 0
+    for n in queens(k, 0):
+        for i in n:
+            buffer.append([c, i])
+            c += 1
+        print(buffer)
+        buffer = []
+        c = 0
+
+NQueens(k)
